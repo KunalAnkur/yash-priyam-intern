8fbab48d04fa61c790f58f7e28988e12086d6d32
Ankur Kunal <mrankur801@gmail.com>
2021-09-08 23:50:47 +0530

    build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const { login, signup, getUser } = require("./routes/userRoutes");
 const authRoute = require("./util/authRoute");
 const {getList,createList,updateList,deleteList} = require("./routes/listRoute");
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "build")));
 
 app.post("/api/signup",signup);
 app.post("/api/login",login);
@@ -24,6 +25,11 @@ app.delete("/api/list/:listId", authRoute, deleteList);
 app.post("/api/card", authRoute, createCard);
 app.put("/api/card", authRoute, updateCard);
 app.delete("/api/card/:listId/:cardId", authRoute, deleteCard);
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 mongoose.connect(process.env.DB_URL).then(() => {
     app.listen(PORT, () => {
         console.log(`server is connect at port ${PORT}`);
